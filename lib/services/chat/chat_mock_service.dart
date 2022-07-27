626f56a931b41010cfad3fd3ae09d863b2a116b9
jermelterzi <jv_ermel@hotmail.com>
2022-07-27 16:44:44 -0300

    Implementing the text field to write new messages

--- lib/services/chat/chat_mock_service.dart
+++ lib/services/chat/chat_mock_service.dart
@@ -6,32 +6,7 @@ import 'package:chat/models/chat_message.dart';
 import 'package:chat/services/chat/chat_service.dart';
 
 class ChatMockService implements ChatService {
-  static final List<ChatMessage> _msgs = [
-    ChatMessage(
-      id: '1',
-      text: 'Bom dia',
-      createdAt: DateTime.now(),
-      userId: '123',
-      userName: 'João',
-      userImageUrl: 'assets/images/avatar.png',
-    ),
-    ChatMessage(
-      id: '2',
-      text: 'Bom dia. Teremos uma reunião hoje?',
-      createdAt: DateTime.now(),
-      userId: '321',
-      userName: 'Gabi',
-      userImageUrl: 'assets/images/avatar.png',
-    ),
-    ChatMessage(
-      id: '3',
-      text: 'Sim. Pode ser agora?',
-      createdAt: DateTime.now(),
-      userId: '123',
-      userName: 'João',
-      userImageUrl: 'assets/images/avatar.png',
-    ),
-  ];
+  static final List<ChatMessage> _msgs = [];
   static MultiStreamController<List<ChatMessage>>? _controller;
   static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
     _controller = controller;
@@ -55,7 +30,7 @@ class ChatMockService implements ChatService {
     );
 
     _msgs.add(newMessage);
-    _controller?.add(_msgs);
+    _controller?.add(_msgs.reversed.toList());
 
     return newMessage;
   }
--- lib/widgets/new_messages.dart
+++ lib/widgets/new_messages.dart
@@ -1,12 +1,62 @@
+import 'package:chat/services/auth/auth_service.dart';
+import 'package:chat/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
-class NewMessages extends StatelessWidget {
+class NewMessages extends StatefulWidget {
   const NewMessages({Key? key}) : super(key: key);
 
   @override
+  State<NewMessages> createState() => _NewMessagesState();
+}
+
+class _NewMessagesState extends State<NewMessages> {
+  late String _message = '';
+  final _messageController = TextEditingController();
+
+  Future<void> _sendMessage() async {
+    final user = AuthService().currentUser;
+
+    if (user != null) {
+      await ChatService().save(_message, user);
+      _messageController.clear();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Nova mensagem'),
+    return Container(
+      color: Theme.of(context).primaryColor,
+      padding: const EdgeInsets.symmetric(
+        vertical: 8.0,
+        horizontal: 4.0,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              onChanged: (msg) => setState(() => _message = msg),
+              decoration: const InputDecoration(
+                labelText: 'Enviar Mensagem...',
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              onSubmitted: (_) {
+                if (_message.trim().isNotEmpty) {
+                  _sendMessage();
+                }
+              },
+            ),
+          ),
+          IconButton(
+            onPressed: _message.trim().isEmpty ? null : _sendMessage,
+            icon: const Icon(
+              Icons.send,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
