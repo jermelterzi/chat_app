3c0d8b9733ac1321c696b0f3f3210707dbd8708d
jermelterzi <jv_ermel@hotmail.com>
2022-07-27 09:52:47 -0300

    Creating a splash page

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,26 @@
+import 'package:chat/models/chat_user.dart';
+import 'package:chat/pages/auth_page.dart';
+import 'package:chat/pages/chat_page.dart';
+import 'package:chat/pages/loading_page.dart';
+import 'package:chat/services/auth/auth_mock_service.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<ChatUser?>(
+        stream: AuthMockService().userChanges,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingPage();
+          } else {
+            return snapshot.hasData ? const ChatPage() : const AuthPage();
+          }
+        },
+      ),
+    );
+  }
+}
