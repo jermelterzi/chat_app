bb377d48ab859bd18f3dbf71e73d26a6a0260fd0
jermelterzi <jv_ermel@hotmail.com>
2022-07-27 09:53:24 -0300

    Creating the chat page

---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,23 @@
+import 'package:chat/services/auth/auth_mock_service.dart';
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Chat Page'),
+            TextButton(
+                onPressed: () => AuthMockService().logout(),
+                child: const Text('Logout'))
+          ],
+        ),
+      ),
+    );
+  }
+}
