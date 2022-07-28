30adbdcb0636719876b4790298ce83495fd1aa1e
jermelterzi <jv_ermel@hotmail.com>
2022-07-28 18:35:17 -0300

    Saving messages in Firestore

---
+++ lib/services/chat/chat_firebase_service.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+
+import 'package:chat/models/chat_user.dart';
+import 'package:chat/models/chat_message.dart';
+import 'package:chat/services/chat/chat_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatFirebaseService implements ChatService {
+  @override
+  Stream<List<ChatMessage>> messagesStream() {
+    return const Stream<List<ChatMessage>>.empty();
+  }
+
+  @override
+  Future<ChatMessage?> save(String text, ChatUser user) async {
+    final store = FirebaseFirestore.instance;
+
+    final msg = ChatMessage(
+      id: '',
+      text: text,
+      createdAt: DateTime.now(),
+      userId: user.id,
+      userName: user.name,
+      userImageUrl: user.imageUrl,
+    );
+
+    // ChatMessage to Map<String, dynamic>
+    final docRef = await store
+        .collection('chat')
+        .withConverter(
+          fromFirestore: _fromFirestore,
+          toFirestore: _toFirestore,
+        )
+        .add(msg);
+
+    final doc = await docRef.get();
+    return doc.data()!;
+  }
+
+  // Map<String, dynamic> to ChatMessage
+  ChatMessage _fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> doc,
+    SnapshotOptions? options,
+  ) {
+    return ChatMessage(
+      id: doc.id,
+      text: doc['text'],
+      createdAt: DateTime.parse(doc['createdAt']),
+      userId: doc['userId'],
+      userName: doc['userName'],
+      userImageUrl: doc['userImageUrl'],
+    );
+  }
+
+  // ChatMessage to Map<String, dynamic>
+  Map<String, dynamic> _toFirestore(
+    ChatMessage msg,
+    SetOptions? options,
+  ) {
+    return {
+      'text': msg.text,
+      'createdAt': msg.createdAt.toIso8601String(),
+      'userId': msg.id,
+      'userName': msg.userName,
+      'userImageUrl': msg.userImageUrl,
+    };
+  }
+}
